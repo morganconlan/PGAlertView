8fab6da311ee99de68c7d1cf9a61215e64af2973
Morgan Conlan <morgan@pangr.ie>
2019-07-09 19:14:36 +0100

    Initial Commit

---
+++ PGAlertView/PGAlertView.h
@@ -0,0 +1,19 @@
+//
+//  PGAlertView.h
+//  PGAlertView
+//
+//  Created by Morgan Conlan on 09/07/2019.
+//  Copyright © 2019 Morgan Conlan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PGAlertView.
+FOUNDATION_EXPORT double PGAlertViewVersionNumber;
+
+//! Project version string for PGAlertView.
+FOUNDATION_EXPORT const unsigned char PGAlertViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PGAlertView/PublicHeader.h>
+
+
